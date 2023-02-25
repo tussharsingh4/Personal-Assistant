0dac2c852dfcbc07827a914e52b606750239787b
Tusshar <tusshar.singh4@gmail.com>
2023-02-25 16:29:26 +0530

    Update server url

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) =>{
 
   loader(messageDiv)
   //fetch data from server ir get the response
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://simpleassist.onrender.com',{
   method: 'POST',
   headers:{
     'Content-Type' : 'application/json'
